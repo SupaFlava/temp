d2ac095c3d609a8bdc59acfb5f020a3db22e9f93
Joey Baetsen <joey@baetsen.net>
2025-05-09 19:52:36 +0200

    cleared current norminette errors in includes/(1 left in here)  and src/lexer/

--- includes/env.h
+++ includes/env.h
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   env.h                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/03 14:39:22 by rmhazres          #+#    #+#             */
-/*   Updated: 2025/05/05 12:35:56 by rmhazres         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   env.h                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rmhazres <rmhazres@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/05/03 14:39:22 by rmhazres      #+#    #+#                 */
+/*   Updated: 2025/05/09 19:28:42 by jbaetsen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
-int     init_env(t_mshell *shell,char **envp);
-void    env_add_back(t_env **shell, t_env *new_node);
+int		init_env(t_mshell *shell, char **envp);
+void	env_add_back(t_env **shell, t_env *new_node);
 
 #endif
--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: jbaetsen <jbaetsen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/06 12:42:53 by jbaetsen      #+#    #+#                 */
-/*   Updated: 2025/05/08 16:34:24 by jbaetsen      ########   odam.nl         */
+/*   Updated: 2025/05/09 19:43:11 by jbaetsen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,16 @@
 
 // function prototypes
 // tokenize_utils.c
-void	handle_default_state(t_state *state, char c, char **buffer, t_token **tokens);
-void	handle_single_quote(t_state *state, char c, char **buffer, t_token **tokens);
-void	handle_double_quote(t_state *state, char c, char **buffer, t_token **tokens);
-void	handle_env_state(t_state *state, char c, char **buffer, t_token **tokens);
-void	handle_redir_state(t_state *state, char c, char **buffer, t_token **tokens);
+void	default_state(t_state *state, char c, char **buffer, t_token **tokens);
+void	s_quote_state(t_state *state, char c, char **buffer, t_token **tokens);
+void	d_quote_state(t_state *state, char c, char **buffer, t_token **tokens);
+void	env_state(t_state *state, char c, char **buffer, t_token **tokens);
+void	redir_state(t_state *state, char c, char **buffer, t_token **tokens);
 
 // lexer.c
-void			handle_char(t_state *state, char c, char **buffer, t_token **tokens);
-void			append_char_to_buffer(char **buffer, char c);
-t_token_type	find_token_type(const char *buffer, t_state state);
-t_token 		*lexer(const char *input);
-void			add_token(t_token **tokens, char *buffer, t_token_type type);
-
+void	handle_char(t_state *state, char c, char **buffer, t_token **tokens);
+void	append_char_to_buffer(char **buffer, char c);
+t_token	*lexer(const char *input);
+void	add_token(t_token **tokens, char *buffer, t_token_type type);
 
 #endif /*LEXER_H*/
