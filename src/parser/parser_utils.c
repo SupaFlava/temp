6f4b0a95a763704fba531cbf36cd0b4997e6e441
Joey Baetsen <joey@baetsen.net>
2025-05-09 20:03:07 +0200

    cleared current norminette errors in src/parser and src/signals

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parser_utils.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/02 15:47:17 by jbaetsen          #+#    #+#             */
-/*   Updated: 2025/05/08 16:03:29 by rmhazres         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   parser_utils.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rmhazres <rmhazres@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/05/02 15:47:17 by jbaetsen      #+#    #+#                 */
+/*   Updated: 2025/05/09 20:01:46 by jbaetsen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char    *read_input(void)
+char	*read_input(void)
 {
-    char    *line;
-    
-    line = readline("> ");
-    if (!line)
-    {
-        ft_printf("exiting minishell\n");
-        exit(0);
-    }
-    return (line);
+	char	*line;
+
+	line = readline("> ");
+	if (!line)
+	{
+		ft_printf("exiting minishell\n");
+		exit(0);
+	}
+	return (line);
 }
