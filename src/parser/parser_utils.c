eef4904bf7f0d5dac0db4949ae81dd6142e5d4e8
Joey Baetsen <joey@baetsen.net>
2025-06-08 14:29:18 +0200

    added char *delimiter to command struct to store the delimiter heredoc needs to look for. adjusted the functions involved

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: rmhazres <rmhazres@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/02 15:47:17 by jbaetsen      #+#    #+#                 */
-/*   Updated: 2025/06/06 15:49:52 by jbaetsen      ########   odam.nl         */
+/*   Updated: 2025/06/08 14:17:51 by jbaetsen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
