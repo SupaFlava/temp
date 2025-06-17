e05f741596c32c2b46e1779d67a7ab1b8f4f642b
Rami Mhazres <rmhazres@f1r6s4.codam.nl>
2025-06-17 13:38:43 +0200

    WIP heredoc refactored

--- src/utils/memory.c
+++ src/utils/memory.c
@@ -6,7 +6,7 @@
 /*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 14:10:03 by rmhazres          #+#    #+#             */
-/*   Updated: 2025/06/16 14:00:23 by rmhazres         ###   ########.fr       */
+/*   Updated: 2025/06/17 13:36:31 by rmhazres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	free_arr(char **arr)
 }
 void	ft_free(t_mshell *shell, t_mem_t type)
 {
+	if (shell->commands->heredoc_temp)
+		unlink(shell->commands->heredoc_temp);
 	if (type == MEM_TEMP)
 	{
 		ft_lstclear(&shell->temp_allocs, free);
